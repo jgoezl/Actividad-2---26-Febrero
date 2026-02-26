0dd77a8155a03ba3403f391cda8c1d39f4ea0653
jgoezl <jgoezl@unal.edu.co>
2026-02-26 00:14:54 -0500

    Create TrianguloRectangulo.java

---
+++ Actividad2.4/src/TrianguloRectangulo.java
@@ -0,0 +1,33 @@
+package actividad2.pkg4;
+public class TrianguloRectangulo {
+    int base;
+    int altura;
+    double hipotenusa;
+    
+    public TrianguloRectangulo(int base, int altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+    
+    double calcularArea() {
+        return (base * altura / 2);
+    }
+    
+    double calcularHipotenusa() {
+        hipotenusa = Math.pow(base*base + altura*altura, 0.5);
+        return Math.round(hipotenusa);
+    }
+    
+    double calcularPerimetro() {
+        return (base + altura + calcularHipotenusa());
+    }
+    
+    String determinarTipoTriangulo() {
+        if ((base == altura) && (base == calcularHipotenusa()) && (altura == calcularHipotenusa())){
+            return "Es un triángulo equilátero";
+        }else if ((base != altura) && (base != calcularHipotenusa()) && (altura != calcularHipotenusa())){
+            return "Es un triángulo escaleno";
+        }else
+            return "Es un triángulo isósceles";
+    }
+}
